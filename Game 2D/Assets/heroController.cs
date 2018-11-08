d103320aabf973848972271e3dffe5156fd52e15
RAEL <happycode.ribeiraoproeto@gmail.com>
2018-11-08 14:37:56 -0200

    Initial commit

---
+++ Game 2D/Assets/heroController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroController : MonoBehaviour {
+
+
+    public CharacterController2D moveController;
+    public float velocidade;
+    public Animator animator;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("up"))
+        {
+            moveController.Move(0, false, true);
+            animator.SetBool("estaPulando", true);
+        }
+        else
+        {
+            animator.SetBool("estaPulando", false);
+        }
+        
+
+        if (Input.GetKey("right"))
+        {
+            moveController.Move(1 * velocidade, false, false);
+            animator.SetBool("estaCorrendo", true);
+        }
+        else
+        {
+            if (Input.GetKey("left"))
+            {
+                moveController.Move(-1 * velocidade, false, false);
+                animator.SetBool("estaCorrendo", true);
+            }
+            else
+            {
+                animator.SetBool("estaCorrendo", false);
+            }
+        }
+
+        
+        
+    }
+ 
+}
