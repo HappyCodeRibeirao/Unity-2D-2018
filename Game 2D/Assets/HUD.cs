647f5ed93cdac3031387ddc26dc4d9bb0d8c224f
RAEL <happycode.ribeiraoproeto@gmail.com>
2018-11-30 21:21:50 -0200

    Review

---
+++ Game 2D/Assets/HUD.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public Sprite[] heartSprite;
+
+    public Image heartUI;
+
+    private int counter;
+
+	void Start ()
+    {
+        counter = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left alt")  && counter<=5)
+        {
+            heartUI.sprite = heartSprite[counter];
+            counter++;
+        }
+    }
+}
---
+++ Game 2D/Assets/HealthUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    int counter = 0;
+    //  Imagem no GUI do JOGO
+    public Image heartUI;
+
+    // Array de imgens
+    public Sprite[] vidasArray = new Sprite[6];
+
+    void Start()
+    {
+        //heartUI.sprite = vidasArray[2];
+
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("left ctrl") && counter < 5)
+        {
+            counter = counter + 1;
+            heartUI.sprite = vidasArray[counter];
+        }
+
+    }
+}
